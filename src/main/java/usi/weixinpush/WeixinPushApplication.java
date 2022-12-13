516a528aeb42974525ed55417ecadc3bb7afc015
cyp-1 <453912020@qq.com>
2022-12-13 16:05:37 +0800

    jar

--- src/main/java/usi/weixinpush/WeixinPushApplication.java
+++ src/main/java/usi/weixinpush/WeixinPushApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeixinPushApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WeixinPushApplication.class, args);
     }
