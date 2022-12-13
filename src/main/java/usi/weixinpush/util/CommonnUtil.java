4b807642362d8e537286970843593d8cea4e7d45
cyp-1 <453912020@qq.com>
2022-12-13 10:36:38 +0800

    jar

--- src/main/java/usi/weixinpush/util/CommonnUtil.java
+++ src/main/java/usi/weixinpush/util/CommonnUtil.java
@@ -2,15 +2,13 @@ package usi.weixinpush.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class CommonnUtil {
-
-    public static void main(String[] args) throws ParseException {
-        System.out.println(calculateFutureDays("2022-09-17"));
-        System.out.println(calculatePastDays("2022-08-16"));
-    }
 
     /**
      * 描述: 获取当前日期
@@ -38,7 +36,7 @@ public class CommonnUtil {
 
 
     /**
-     * 描述: 计算还有多少天
+     * 描述: 指定日期距今还有多少天
      * @Author wangxianlin
      * @Date 22:21 2022/8/20
      * @param: day
@@ -53,13 +51,33 @@ public class CommonnUtil {
         return String.valueOf(time/1000/60/60/24);
     }
 
-
     /**
-     * 描述: 计算已经过去了多少天
+     * 描述: 生日还有多少天
      * @Author wangxianlin
      * @Date 22:21 2022/8/20
      * @param: day
      * @Return java.lang.String
+     */
+    public static String calculateBirth(String day) throws ParseException {
+        if(!day.endsWith("00:00:00")){
+            day += " 00:00:00";
+        }
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime timeDate = LocalDateTime.parse(day, formater);
+        while(now.compareTo(timeDate)>0){
+            timeDate = timeDate.plusYears(1);
+        }
+        long interval = Duration.between(now,timeDate).toMillis();
+        return String.valueOf(interval/1000/60/60/24);
+    }
+
+    public static void main(String[] args) throws ParseException {
+        System.out.println(calculateBirth(("2021-12-04 00:00:00")));
+    }
+
+    /**
+     * 描述: 计算已经过去了多少天
      */
     public static String calculatePastDays(String day) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
