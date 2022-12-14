384a9d7d344ffbdb796dffcbe3debf1c95c6c669
cyp-1 <453912020@qq.com>
2022-12-14 13:46:31 +0800

    补充 阴历生日

--- src/main/java/usi/weixinpush/service/WeiXinTemplateService.java
+++ src/main/java/usi/weixinpush/service/WeiXinTemplateService.java
@@ -12,6 +12,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.ParseException;
 
+import static usi.weixinpush.constant.WeatherForecast.*;
+
 public class WeiXinTemplateService {
 
     private static Logger log = LoggerFactory.getLogger(WeiXinTemplateService.class);
@@ -24,9 +26,9 @@ public class WeiXinTemplateService {
     public static final   String APP_ID = ConfigUtil.getValue("APP_ID");
     public static final  String APP_SECRET = ConfigUtil.getValue("APP_SECRET");
     /** 发送给谁 OPENID */
-    public static final String TO_USER = ConfigUtil.getValue("TO_USER");
+    public static final String TO_USER[] = ConfigUtil.getValue("TO_USER").split(",");
     /** 推送的模板ID */
-    public static final String TEMPLATE_ID = ConfigUtil.getValue("TEMPLATE_ID");
+    public static final String TEMPLATE_ID[] = ConfigUtil.getValue("TEMPLATE_ID").split(",");
 
     /**
      * 描述: 获取用户Token
@@ -53,22 +55,17 @@ public class WeiXinTemplateService {
     public static void pushWeiXinTemplate(){
         String accessTokenUrl = PUSH_TEMPLATE_URL + getAccessToken();
         JSONObject json = new JSONObject();
-        // 接收用户的OPENID
-        json.put("touser",TO_USER);
-        // 模板ID
-        json.put("template_id",TEMPLATE_ID);
-        // 消息唯一标识
-        json.put("client_msg_id",System.currentTimeMillis()+"");
+
         // 内容
         JSONObject data = new JSONObject();
         data.put("today",getJSON(CommonnUtil.getNow(),"#E9FB8A"));
-        data.put("city",getJSON("湛江","#0080FF"));
+        data.put("city",getJSON(livePlace,"#0080FF"));
         // 距离我的生日
         try {
 //            data.put("a",getJSON(CommonnUtil.calculatePastDays("2022-08-16"),"#A3C6FC"));
-            data.put("withU",getJSON(CommonnUtil.calculatePastDays("2021-11-20"),"#A3C6FC"));
-            data.put("sheBir",getJSON(CommonnUtil.calculateBirth("2000-01-22"),"#FB2413"));
-            data.put("myBir",getJSON(CommonnUtil.calculateBirth("1998-11-15"),"#DEFDAB "));
+            data.put("withU",getJSON(CommonnUtil.calculatePastDays(withTime),"#A3C6FC"));
+            data.put("sheBir",getJSON(CommonnUtil.calculateBirth(sheBir),"#FB2413"));
+            data.put("myBir",getJSON(CommonnUtil.calculateBirth(myBir),"#DEFDAB "));
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -76,6 +73,8 @@ public class WeiXinTemplateService {
         data.put("tuQ",getJSON(getEarthyLoveStory3(),"#FDE050"));
         // 笑话
         data.put("joK",getJSON(getJokes(),"#44b41f"));
+        // 励志英语
+        data.put("English",getJSON(getInspiringEnglish(),"#c1792c"));
         // 获取每日天气
         try {
             JSONObject weatherJson = getEveryWeather();
@@ -95,10 +94,26 @@ public class WeiXinTemplateService {
             e.printStackTrace();
         }
         json.put("data",data);
-        log.info("请求数据：[{}]",json.toString());
-        String res = HttpClientUtil.doPost(accessTokenUrl,json);
-        JSONObject jsonRes = JSON.parseObject(res);
-        log.info("获取模板列表 返回数据：[{}]",jsonRes.toString());
+        // 接收用户的OPENID
+        for (String toUser : TO_USER){
+            json.put("touser",toUser);
+            for (String template_id : TEMPLATE_ID){
+                // 模板ID
+                json.put("template_id",template_id);
+                // 消息唯一标识
+                json.put("client_msg_id",System.currentTimeMillis()+"");
+                // 调用vx API
+                log.info("请求数据：[{}]",json.toString());
+                String res = HttpClientUtil.doPost(accessTokenUrl,json);
+                JSONObject jsonRes = JSON.parseObject(res);
+                log.info("获取模板列表 返回数据：[{}]",jsonRes.toString());
+                try {
+                    Thread.sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     /**
@@ -112,9 +127,22 @@ public class WeiXinTemplateService {
         return onlyJok;
     }
 
+    /**
+     * 描述:  获取笑话
+     */
+    public static String getInspiringEnglish(){
+        String tu_url = "https://api.vvhan.com/api/en?type=sj";
+        String res = HttpClientUtil.doGet(tu_url,"UTF-8");
+        JSONObject jsonObject = JSON.parseObject(res);
+        String zh = JSON.parseObject(jsonObject.getString("data")).getString("zh");
+        String en = JSON.parseObject(jsonObject.getString("data")).getString("en");
+//        String onlyJok = res.replaceAll(".*\"joke\":\"(.*)\".*?","$1");
+//        log.info("获取笑话 返回数据：[{}]",onlyJok);
+        return zh+"\n"+en;
+    }
+
     public static void main(String[] args) {
-        getJokes();
-        getEarthyLoveStory3();
+        System.out.println(getInspiringEnglish());
     }
 
     /**
