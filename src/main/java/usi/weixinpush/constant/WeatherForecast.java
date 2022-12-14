04ab56c2c13ba641abf198a48c854c4de57a86cd
cyp-1 <453912020@qq.com>
2022-12-14 13:48:33 +0800

    补充 阴历生日

---
+++ src/main/java/usi/weixinpush/constant/WeatherForecast.java
@@ -0,0 +1,16 @@
+package usi.weixinpush.constant;
+
+/**
+ * @author cyp
+ * @create 2022-12-14 13:00
+ */
+public interface WeatherForecast {
+    // 居住地点
+    String livePlace = "湛江";
+    // 在一起时间
+    String withTime = "2021-11-20";
+    // 她的生日
+    String sheBir = "2000-01-22";
+    // 我的生日
+    String myBir = "1998-11-15";
+}
