604de4a7c5868b4eb6de7f07854016d0e2ef7ad9
cyp-1 <453912020@qq.com>
2022-12-14 11:15:24 +0800

    补充 阴历生日

--- src/main/java/usi/weixinpush/job/SaticScheduleTask.java
+++ src/main/java/usi/weixinpush/job/SaticScheduleTask.java
@@ -1,5 +1,8 @@
 package usi.weixinpush.job;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,8 +12,13 @@ import static usi.weixinpush.service.WeiXinTemplateService.pushWeiXinTemplate;
 @Configuration
 @EnableScheduling
 public class SaticScheduleTask {
+    @Autowired
+    private ApplicationContext appContext;
+    public void initiateShutdown(int returnCode){
+        SpringApplication.exit(appContext, () -> returnCode);
+    }
 
-    @Scheduled(cron = "0/30 0 8 * * ?")
+    @Scheduled(cron = "0/20 * * * * ?")
     private void configureTasks() {
         pushWeiXinTemplate();
     }
